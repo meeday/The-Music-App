671f9387fa7a9be66332ba0bcc2f9843093de80d
Meeday <meedaxa.ahmed@gmail.com>
2020-04-30 16:05:19 +0100

    html-landing-page

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,3 @@
-var instances;
-document.addEventListener('DOMContentLoaded', function() {
- var elems = document.querySelectorAll('select');
- const options = [
- { name : 'Album',  value : 'album'},
- { name : 'Artist',  value : 'artist'},
- { name : 'Track',  value : 'track'},
- ]
-  instances = M.FormSelect.init(elems, options);
-});
+
     
 
