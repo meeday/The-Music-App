bf05af530c38761fc2c118ac1db057c4594adc7c
Meeday <meedaxa.ahmed@gmail.com>
2020-04-30 14:54:37 +0100

    improved landing page

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
+var instances;
+document.addEventListener('DOMContentLoaded', function() {
+ var elems = document.querySelectorAll('select');
+ const options = [
+ { name : 'Album',  value : 'album'},
+ { name : 'Artist',  value : 'artist'},
+ { name : 'Track',  value : 'track'},
+ ]
+  instances = M.FormSelect.init(elems, options);
+});
+    
 
