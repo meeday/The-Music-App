ef09e74a4b0387df5ff09bc219ebeada4ce84bbe
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-01 15:20:25 +0100

    Grouped event listeners into document.ready

--- assets/script.js
+++ assets/script.js
@@ -4,17 +4,9 @@
 
 var userInput;
 
-$("#search-icon").on("click", function () {
-  var userInput = $("#search-query").val();
-  console.log("user input is " + userInput);
-  callAPI(userInput);
-});
-
 function callAPI() {
   if ($("#track-btn").is(":checked")) {
     getTrackInfo(userInput);
-  } else if ($("#album-btn").is(":checked")) {
-    getAlbumInfo(userInput);
   } else if ($("#artist-btn").is(":checked")) {
     getArtistInfo(userInput);
   }
@@ -35,17 +27,20 @@ function getTrackInfo(userInput) {
   });
 }
 
-function getAlbumInfo(userInput) {
-  userInput = $("#search-query").val();
+function getAlbumInfo() {
+  var artist = $("#query-artist").val();
+  var album = $("query album").val();
   let albumURL =
-    "http://ws.audioscrobbler.com/2.0/?method=album.search&album=" +
-    userInput +
-    "&api_key=1cdcc6e0cda44cee6b6571363c390279&format=json";
+    "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=1cdcc6e0cda44cee6b6571363c390279&artist=" +
+    artist +
+    "&album=" +
+    album +
+    "&format=json";
   $.ajax({
     url: albumURL,
     method: "GET",
   }).then(function (response) {
-    console.log("user searched for album: " + userInput);
+    console.log("user searched for " + album + " by " + artist);
     console.log(response);
   });
 }
@@ -65,14 +60,20 @@ function getArtistInfo(userInput) {
   });
 }
 
-$(function(){
-    $('#album-btn').on('click', function(){
-        $('#default-search-input').hide(400);
-        $('#album-search-input').show(400);
-    })
-    
-    $('.default-query').on('click', function(){
-        $('#default-search-input').show(400);
-        $('#album-search-input').hide(400);
-    })
-})
+$(function () {
+  $("#search-icon").on("click", function () {
+    var userInput = $("#search-query").val();
+    console.log("user input is " + userInput);
+    callAPI(userInput);
+  });
+
+  $("#album-btn").on("click", function () {
+    $("#default-search-input").hide(400);
+    $("#album-search-input").show(400);
+  });
+
+  $(".default-query").on("click", function () {
+    $("#default-search-input").show(400);
+    $("#album-search-input").hide(400);
+  });
+});
