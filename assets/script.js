ee425a009977d8ccc255486b62294a0f2bf20b6f
meeday <59993824+meeday@users.noreply.github.com>
2020-05-03 19:39:55 +0100

    added modal links to tracks on album-search page

--- assets/script.js
+++ assets/script.js
@@ -83,7 +83,7 @@ function getAlbumInfo() {
     }
     var tracks = response.album.tracks.track;
     for (i = 0; i < tracks.length; i++) {
-      $(".search-tracks>ol").append("<li>" + tracks[i].name + "</li>");
+      $(".search-tracks>ol").append("<li><a id='tracks' class='waves-effect waves-light collection-item modal-trigger' href='#track-modal'>" + tracks[i].name + "</a></li>");
     }
   });
   $("#album-results-page").show(400);
