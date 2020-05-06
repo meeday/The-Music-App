126a323126835a65501ce73780ed45c4c75849aa
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-05-06 14:10:31 +0100

    format2

--- assets/script.js
+++ assets/script.js
@@ -5,55 +5,55 @@
 var userInput;
 
 function callAPI() {
-  if ($('#track-btn').is(':checked')) {
-    $('#album-input>label').toggle();
-    $('#artist-input>label').toggle();
+  if ($("#track-btn").is(":checked")) {
+    $("#album-input>label").toggle();
+    $("#artist-input>label").toggle();
     getTrackInfo(userInput);
-  } else if ($('#artist-btn').is(':checked')) {
-    $('#album-input>label').toggle();
-    $('#artist-input>label').toggle();
+  } else if ($("#artist-btn").is(":checked")) {
+    $("#album-input>label").toggle();
+    $("#artist-input>label").toggle();
     getArtistInfo(userInput);
-  } else if ($('#album-btn').is(':checked')) {
-    $('#album-input>label').toggle();
-    $('#artist-input>label').toggle();
+  } else if ($("#album-btn").is(":checked")) {
+    $("#album-input>label").toggle();
+    $("#artist-input>label").toggle();
     getAlbumInfo();
   }
 }
 
 function getTrackInfo(userInput) {
-  userInput = $('#search-query').val();
+  userInput = $("#search-query").val();
   var trackURL =
-    'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' +
+    "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" +
     userInput +
-    '&api_key=1cdcc6e0cda44cee6b6571363c390279&format=json';
+    "&api_key=1cdcc6e0cda44cee6b6571363c390279&format=json";
   $.ajax({
     url: trackURL,
-    method: 'GET',
+    method: "GET",
     // Handling errors with the api call
     error: function (xhr, _ajaxOptions, _thrownError) {
       if (xhr.status == 400) {
         M.toast({
-          html: '400: Invalid Input.',
-          classes: 'error-message',
+          html: "400: Invalid Input.",
+          classes: "error-message",
         });
       } else if (xhr.status == 404) {
-        M.toast({ html: '404: Not Found.', classes: 'error-message' });
+        M.toast({ html: "404: Not Found.", classes: "error-message" });
       }
     },
   }).then(function (response) {
     if (response.error) {
       var error = response.message;
-      M.toast({ html: error, classes: 'error-message' });
-    } else if (response.results['opensearch:totalResults'] == '0') {
+      M.toast({ html: error, classes: "error-message" });
+    } else if (response.results["opensearch:totalResults"] == "0") {
       M.toast({
-        html: 'The track you supplied could not be found',
-        classes: 'error-message',
+        html: "The track you supplied could not be found",
+        classes: "error-message",
       });
     } else {
       //Track search result shown
       var firstTrackName = response.results.trackmatches.track[0].name;
       var tracks = response.results.trackmatches.track;
-      $('#track-name').text(firstTrackName);
+      $("#track-name").text(firstTrackName);
       //if function to ensure max show track is 10
       if (tracks.length > 10) {
         var length = 10;
@@ -61,181 +61,181 @@ function getTrackInfo(userInput) {
         var length = tracks.length;
       }
       //reset search result list
-      $('#track-search-result>ol').html('');
+      $("#track-search-result>ol").html("");
       //build up search result list
       for (i = 0; i < length; i++) {
-        $('#track-search-result>ol').append(
+        $("#track-search-result>ol").append(
           "<li><a id='tracks' class='track-result waves-effect waves-light collection-item modal-trigger' href='#track-modal'>" +
             tracks[i].name +
-            ' - ' +
+            " - " +
             tracks[i].artist +
-            '</a></li>'
+            "</a></li>"
         );
       }
       //show the result page after finish call
-      $('#track-results-page').show(400);
+      $("#track-results-page").show(400);
     }
     //"reading" the value of <li>
-    $('.track-result').on('click', function () {
+    $(".track-result").on("click", function () {
       var track = $(this).text();
       //Create lyrics api url with <li> information. The syntax is //https://api.audd.io/findLyrics/?q=adele hello
       var lyricsURL =
-        'https://api.audd.io/findLyrics/?q=' +
+        "https://api.audd.io/findLyrics/?q=" +
         track +
-        '&api_token=56504f66202634311b0e5c04f32ced06';
+        "&api_token=56504f66202634311b0e5c04f32ced06";
       $.ajax({
         url: lyricsURL,
-        method: 'GET',
+        method: "GET",
       }).then(function (response) {
         var lyrics = response.result[0].lyrics;
-        $('#modal-track-search-result').text(lyrics);
+        $("#modal-track-search-result").text(lyrics);
       });
     });
   });
 }
 
 function getAlbumInfo() {
-  var artist = $('#query-artist').val();
-  var album = $('#query-album').val();
+  var artist = $("#query-artist").val();
+  var album = $("#query-album").val();
   var albumURL =
-    'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=1cdcc6e0cda44cee6b6571363c390279&artist=' +
+    "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=1cdcc6e0cda44cee6b6571363c390279&artist=" +
     artist +
-    '&album=' +
+    "&album=" +
     album +
-    '&format=json';
+    "&format=json";
   $.ajax({
     url: albumURL,
-    method: 'GET',
+    method: "GET",
     // Handling errors with the api call
     error: function (xhr, _ajaxOptions, _thrownError) {
       if (xhr.status == 400) {
         M.toast({
-          html: '400: Invalid Input.',
-          classes: 'error-message',
+          html: "400: Invalid Input.",
+          classes: "error-message",
         });
       } else if (xhr.status == 404) {
-        M.toast({ html: '404: Not Found.', classes: 'error-message' });
+        M.toast({ html: "404: Not Found.", classes: "error-message" });
       }
     },
   }).then(function (response) {
     // Checking for error response
     if (response.error) {
       var error = response.message;
-      M.toast({ html: error, classes: 'error-message' });
-      $('#toast-container').css('top', '44%');
+      M.toast({ html: error, classes: "error-message" });
+      $("#toast-container").css("top", "44%");
     } else {
       var albumName = response.album.name;
-      var icon = response.album.image[2]['#text'];
+      var icon = response.album.image[2]["#text"];
       var tracks = response.album.tracks.track;
       // Checking if an empty album was returned
       if (!icon && !tracks.length) {
         M.toast({
-          html: 'The album you supplied could not be found',
-          classes: 'error-message',
+          html: "The album you supplied could not be found",
+          classes: "error-message",
         });
-        $('#toast-container').css('top', '44%');
+        $("#toast-container").css("top", "44%");
       } else {
         //album search result shown
-        $('#album-pic').attr('src', icon);
-        $('#summaryHeading').text(albumName);
-        $('.search-tracks>ol').html('');
+        $("#album-pic").attr("src", icon);
+        $("#summaryHeading").text(albumName);
+        $(".search-tracks>ol").html("");
         if (!response.album.wiki) {
-          $('#summary').hide();
+          $("#summary").hide();
         } else {
-          $('#summary').html(response.album.wiki.summary);
-          $('#summary').show();
+          $("#summary").html(response.album.wiki.summary);
+          $("#summary").show();
         }
         for (i = 0; i < tracks.length; i++) {
-          $('.search-tracks>ol').append(
+          $(".search-tracks>ol").append(
             "<li><a id='tracks' class='waves-effect waves-light collection-item modal-trigger' href='#track-modal'>" +
               tracks[i].name +
-              '</a></li>'
+              "</a></li>"
           );
         }
-        $('#album-results-page').show(400);
+        $("#album-results-page").show(400);
       }
     }
   });
 }
 
 function getArtistInfo(userInput) {
-  userInput = $('#search-query').val();
+  userInput = $("#search-query").val();
   var artistURL =
-    'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' +
+    "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" +
     userInput +
-    '&api_key=1cdcc6e0cda44cee6b6571363c390279&format=json';
+    "&api_key=1cdcc6e0cda44cee6b6571363c390279&format=json";
   $.ajax({
     url: artistURL,
-    method: 'GET',
+    method: "GET",
     // Handling errors with the api call
     error: function (xhr, _ajaxOptions, _thrownError) {
       if (xhr.status == 400) {
         M.toast({
-          html: '400: Invalid Input.',
-          classes: 'error-message',
+          html: "400: Invalid Input.",
+          classes: "error-message",
         });
       } else if (xhr.status == 404) {
-        M.toast({ html: '404: Not Found.', classes: 'error-message' });
+        M.toast({ html: "404: Not Found.", classes: "error-message" });
       }
     },
   }).then(function (response) {
     // Handling LastFM error response
     if (response.error) {
       var error = response.message;
-      M.toast({ html: error, classes: 'error-message' });
+      M.toast({ html: error, classes: "error-message" });
     } else {
       // Determining if artist exists
       var name = response.artist.name;
       var bio = response.artist.bio;
-      var image = response.artist.image[0]['#text'];
+      var image = response.artist.image[0]["#text"];
       if (!bio.content && !image) {
         M.toast({
-          html: 'The artist you supplied could not be found',
-          classes: 'error-message',
+          html: "The artist you supplied could not be found",
+          classes: "error-message",
         });
       } else {
-        $('#artist-name').text(name);
-        $('#artist-bio').html(bio.summary);
-        $('#artist-bio>a').attr('Target', '_blank');
+        $("#artist-name").text(name);
+        $("#artist-bio").html(bio.summary);
+        $("#artist-bio>a").attr("Target", "_blank");
         // Filling top 4 albums
         var topAlbumURL =
-          'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' +
+          "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" +
           userInput +
-          '&api_key=1cdcc6e0cda44cee6b6571363c390279&format=json';
+          "&api_key=1cdcc6e0cda44cee6b6571363c390279&format=json";
 
         $.ajax({
           url: topAlbumURL,
-          method: 'GET',
+          method: "GET",
         }).then(function (response) {
           console.log(response);
           //iterating through albums for a header image
           for (i = 0; i < 50; i++) {
-            var albumImage = response.topalbums.album[i].image[2]['#text'];
+            var albumImage = response.topalbums.album[i].image[2]["#text"];
             var albumName = response.topalbums.album[i].name;
             if (!albumImage || !albumName) {
               continue;
             } else {
-              $('#header-img').attr('src', albumImage);
-              $('#header-img').attr('alt', albumName);
-              $('#albums>ul').html('');
+              $("#header-img").attr("src", albumImage);
+              $("#header-img").attr("alt", albumName);
+              $("#albums>ul").html("");
               break;
             }
           }
           for (i = 0, a = 0; i < 50, a < 4; i++) {
-            var albumImage = response.topalbums.album[i].image[2]['#text'];
+            var albumImage = response.topalbums.album[i].image[2]["#text"];
             var albumName = response.topalbums.album[i].name;
             if (!albumImage || !albumName) {
               // Checking if album has both img and alt text
               continue;
             } else {
               // Appending the album
-              $('#albums>ul').append(
+              $("#albums>ul").append(
                 '<li><a id="albums" class="waves-effect waves-light modal-trigger" href="#album-modal"><img src="' +
                   albumImage +
                   'alt="' +
                   albumName +
-                  '/></a>' +
-                  '</li>'
+                  "/></a>" +
+                  "</li>"
               );
               // Incrimenting album count
               a++;
@@ -245,25 +245,25 @@ function getArtistInfo(userInput) {
 
         // Getting top tracks
         var topTrackURL =
-          'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=' +
+          "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" +
           userInput +
-          '&api_key=1cdcc6e0cda44cee6b6571363c390279&format=json';
+          "&api_key=1cdcc6e0cda44cee6b6571363c390279&format=json";
 
         $.ajax({
           url: topTrackURL,
-          method: 'GET',
+          method: "GET",
         }).then(function (response) {
-          $('#top-tracks>ol').html('');
+          $("#top-tracks>ol").html("");
           for (i = 0; i < 5; i++) {
-            $('#top-tracks>ol').append(
+            $("#top-tracks>ol").append(
               '<li><a id="tracks" class="waves-effect waves-light collection-item modal-trigger" href="#track-modal">' +
-                '<span>' +
+                "<span>" +
                 response.toptracks.track[i].name +
-                '</span></a></li>'
+                "</span></a></li>"
             );
           }
         });
-        $('#artist-results-page').show(400);
+        $("#artist-results-page").show(400);
       }
     }
   });
@@ -271,13 +271,13 @@ function getArtistInfo(userInput) {
 
 //These apply on page load
 $(function () {
-  $('#search-icon').on('click', function () {
-    var userInput = $('#search-query').val();
-    $('.result-page').hide(400);
+  $("#search-icon").on("click", function () {
+    var userInput = $("#search-query").val();
+    $(".result-page").hide(400);
     callAPI(userInput);
-    $('#default-search-input>label').remove();
-    $('#album-input>label').toggle();
-    $('#artist-input>label').toggle();
+    $("#default-search-input>label").remove();
+    $("#album-input>label").toggle();
+    $("#artist-input>label").toggle();
   });
 
   // Event listeners
@@ -285,36 +285,36 @@ $(function () {
   // Search on <enter key> pressed
   $(document).keypress(function (event) {
     var keycode = event.keyCode ? event.keyCode : event.which;
-    if (keycode == '13') {
-      $('.result-page').hide(400);
+    if (keycode == "13") {
+      $(".result-page").hide(400);
       callAPI();
-      $('#default-search-input>label').remove();
-      $('#album-input>label').toggle();
-      $('#artist-input>label').toggle();
+      $("#default-search-input>label").remove();
+      $("#album-input>label").toggle();
+      $("#artist-input>label").toggle();
     }
   });
 
   // Toggling the label for the search bar on search
-  $('#album-search-icon').on('click', function () {
-    $('#album-input>label').toggle();
-    $('#artist-input>label').toggle();
-    $('.result-page').hide(400);
+  $("#album-search-icon").on("click", function () {
+    $("#album-input>label").toggle();
+    $("#artist-input>label").toggle();
+    $(".result-page").hide(400);
     getAlbumInfo();
   });
 
   // Changing inputs for album search to incorporate artist input
-  $('#album-btn').on('click', function () {
-    $('#default-search-input').hide(400);
-    $('#album-search-input').show(400);
+  $("#album-btn").on("click", function () {
+    $("#default-search-input").hide(400);
+    $("#album-search-input").show(400);
   });
 
-  $('.default-query').on('click', function () {
-    $('#default-search-input').show(400);
-    $('#album-search-input').hide(400);
+  $(".default-query").on("click", function () {
+    $("#default-search-input").show(400);
+    $("#album-search-input").hide(400);
   });
 });
 // Modal function
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = $('.modal');
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = $(".modal");
   var instances = M.Modal.init(elems);
 });
